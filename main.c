a18b1c2699bfbe1ae22ea110f01028703100646f
이세인 / Sein Lee <lsin07@naver.com>
2023-11-13 16:13:33 +0900

    init.h removed

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include "clocks_and_modes.h"
 #include "ADC.h"
 #include "LPUART.h"
-#include "init.h"
 
 void delay_ms(volatile int ms);
+void PORT_init(void);
+void WDOG_disable(void);
+void LPIT0_init(uint32_t);
 int lpit0_ch0_flag_counter = 0;
 
 int main(void)
@@ -52,6 +54,65 @@ int main(void)
 		LPUART1_transmit_word(n);
 		delay_ms(100);
 	}
+}
+
+void PORT_init(void)
+{
+	PCC->PCCn[PCC_PORTC_INDEX] = PCC_PCCn_CGC_MASK;
+	PORTC->PCR[6] &= PORT_PCR_MUX(7);
+	PORTC->PCR[6] |= PORT_PCR_MUX(2);
+	PORTC->PCR[7] &= PORT_PCR_MUX(7);
+	PORTC->PCR[7] |= PORT_PCR_MUX(2);
+	PORTC->PCR[12] &= PORT_PCR_MUX(7);
+	PORTC->PCR[12] |= PORT_PCR_MUX(1);
+	PORTC->PCR[13] &= PORT_PCR_MUX(7);
+	PORTC->PCR[13] |= PORT_PCR_MUX(1);
+	PTC->PDDR &= ~((1 << 12) | (1 << 13));
+
+	PCC->PCCn[PCC_PORTD_INDEX] = PCC_PCCn_CGC_MASK;
+	PORTD->PCR[0] &= PORT_PCR_MUX(7);
+	PORTD->PCR[0] |= PORT_PCR_MUX(1);
+	PORTD->PCR[15] &= PORT_PCR_MUX(7);
+	PORTD->PCR[15] |= PORT_PCR_MUX(1);
+	PTD->PDDR |= 1 | (1 << 15);
+}
+
+void WDOG_disable(void)
+{
+	WDOG->CNT = 0xD928C520;	  /* Unlock watchdog 		*/
+	WDOG->TOVAL = 0x0000FFFF; /* Maximum timeout value 	*/
+	WDOG->CS = 0x00002100;	  /* Disable watchdog 		*/
+}
+
+void LPIT0_init(uint32_t delay)
+{
+	uint32_t timeout;
+	/*!
+	 * LPIT Clocking:
+	 * ==============================
+	 */
+	PCC->PCCn[PCC_LPIT_INDEX] = PCC_PCCn_PCS(6);	/* Clock Src = 6 (SPLL2_DIV2_CLK)*/
+	PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_CGC_MASK; /* Enable clk to LPIT0 regs       */
+
+	/*!
+	 * LPIT Initialization:
+	 */
+	LPIT0->MCR |= LPIT_MCR_M_CEN_MASK; /* DBG_EN-0: Timer chans stop in Debug mode */
+									   /* DOZE_EN=0: Timer chans are stopped in DOZE mode */
+									   /* SW_RST=0: SW reset does not reset timer chans, regs */
+									   /* M_CEN=1: enable module clk (allows writing other LPIT0 regs) */
+
+	timeout = delay * 40000;
+	LPIT0->TMR[0].TVAL = timeout; /* Chan 0 Timeout period: 40M clocks */
+	LPIT0->TMR[0].TCTRL |= LPIT_TMR_TCTRL_T_EN_MASK;
+	/* T_EN=1: Timer channel is enabled */
+	/* CHAIN=0: channel chaining is disabled */
+	/* MODE=0: 32 periodic counter mode */
+	/* TSOT=0: Timer decrements immediately based on restart */
+	/* TSOI=0: Timer does not stop after timeout */
+	/* TROT=0 Timer will not reload on trigger */
+	/* TRG_SRC=0: External trigger soruce */
+	/* TRG_SEL=0: Timer chan 0 trigger source is selected*/
 }
 
 void delay_ms(volatile int ms)
